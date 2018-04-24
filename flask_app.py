919984f89fb4fe87918f39333eca5231a775e53a
jsaromo <jonathansaromo@24dcaea7d349>
2018-04-24 15:52:42 +0000

    Converted class schedule page to a template.

--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,26 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
+import constants
+
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def homepage():
     return 'Hello from Jonathan!'
 
+@app.route('/aboutme')
 @app.route('/about_me')
 def about_me():
     return app.send_static_file('about_me.html')
 
 @app.route('/class_schedule')
 def class_schedule():
-    return app.send_static_file('class_schedule.html')
+    return render_template('class_schedule.html',
+                           courses=constants.COURSES)
 
 @app.route('/register')
 def register():
