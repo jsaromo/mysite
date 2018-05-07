996346bb2a13ca7f434ad16552aff3a8f1be584c
Jonathan Saromo <jonathansaromo@24dcaea7d349>
2018-05-07 15:53:28 +0000

    Created song table and imported data

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,3 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 import constants
@@ -18,6 +17,12 @@ class Course(db.Model):
     teacher_name = db.Column(db.String(80))
     resource_name = db.Column(db.String(80))
     resource_url = db.Column(db.String(300))
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    artist_name = db.Column(db.String(80))
+    youtube_url = db.Column(db.String(300))
 
 @app.route('/')
 def homepage():
@@ -40,6 +45,7 @@ def register():
 
 @app.route('/top_ten_songs')
 def top_ten_songs():
+    songs = Song.query.all()
     return render_template('top_ten_songs.html',
                             songs=constants.TOP_TEN_SONGS)
 
